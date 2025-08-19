ac4cdfe98d953c1d2e84b46e06e01c375126479a
itsYuuuka <okan_altun@hotmail.com>
2025-08-20 01:35:59 +0200

    Add serialized float fields for x, y, z movement values

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
