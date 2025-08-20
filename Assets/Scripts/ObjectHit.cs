f6a596ea2f16fc38931dd7e8c8fb7dd405501764
itsYuuuka <okan_altun@hotmail.com>
2025-08-20 20:21:51 +0200

    Remove debug log

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -5,6 +5,5 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         GetComponent<MeshRenderer>().material.color = Color.black;
-        Debug.Log("Player has hit an object!");
     }
 }
