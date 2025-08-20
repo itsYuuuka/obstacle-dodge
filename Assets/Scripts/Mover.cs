857ba06be266f13b4223cec3acd07896b9ffc0d8
itsYuuuka <okan_altun@hotmail.com>
2025-08-20 17:48:38 +0200

    Add player instructions and refactor movement code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,11 +7,23 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move with WASD or Arrow keys.");
+        Debug.Log("Dont bump into objects!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
