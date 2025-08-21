2d10277d948ce9f8527c2ce49154717d52f46ea9
itsYuuuka <okan_altun@hotmail.com>
2025-08-21 14:23:35 +0200

    Refactor collision handling in ObjectHit and Scorer scripts to ensure color change and hit tracking only occur on player collisions

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 {
-   int hits = 0; 
+    int hits = 0;
 
-   private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("Player bumped " + hits + " times into an obstacle!");
+        if (other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("Player bumped " + hits + " times into an obstacle!");
+        }
     }
 }
