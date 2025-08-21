f056da876dd6d587e9aca1e786bcade36c3c0871
itsYuuuka <okan_altun@hotmail.com>
2025-08-21 13:50:38 +0200

    Add configurable drop timer to Dropper component

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] float droptTime = 2f;
+
     void Start()
     {
         
@@ -9,6 +11,9 @@ public class Dropper : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(Time.time);
+        if (Time.time > droptTime)
+        {
+            Debug.Log("Dropping object after 3 seconds");
+        }
     }
 }
